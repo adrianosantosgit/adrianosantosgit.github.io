345f64b18dfdf576df1fa825265b838bc5db8ee6
Adriano Santos <adriansants@msn.com>
2025-05-03 21:53:23 -0300

    profile page update init

---
+++ v1/script.js
@@ -0,0 +1,9 @@
+function lightMode() {
+    const el = document.querySelector("link");
+    el.href = "light.css";
+}
+function darkMode() {
+    const el = document.querySelector("link");
+    el.href = "dark.css";
+}
+
